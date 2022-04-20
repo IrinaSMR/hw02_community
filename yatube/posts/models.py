5ecad3d2d994401f9ad2f89511676b466ce037ba
Ирина Смирнова <ismirnova83@yandex.ru>
2022-04-20 19:00:46 +0300

    Updated

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+
 class Group(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(primary_key=True)
